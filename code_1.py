5fd3fb219e16225e31fea87801dc907bb1ae7854
garret14deras <139816612+garret14deras@users.noreply.github.com>
2023-09-22 02:09:11 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,14 @@
+    for item in data:
+import random
+
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+if __name__ == "__main__":
+    main()
+        print(f"Random Number: {item}")
+    return data
+
+
+def main():
